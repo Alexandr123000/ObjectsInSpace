573e7871e58034257ca8294d3d1c9020a4ce331e
Alexandr123000 <alexandrt10000@gmail.com>
2026-07-25 21:12:52 +0300

    WIP: development process

---
+++ scripts.js
@@ -0,0 +1,31 @@
+/*some code
+
+@media (max-width: 374px)
+{
+
+}
+@media (min-width: 375px) and (max-width: 479px)
+{
+
+}
+@media (min-width: 480px) and (max-width: 767px)
+{
+
+}
+@media (min-width: 758px) and (max-width: 991px)
+{
+
+}
+@media (min-width: 992px) and (max-width: 1199px)
+{
+
+}
+@media (min-width: 1200px) and (max-width: 1439px)
+{
+
+}
+@media (mim-width: 1440px)
+{
+
+}
+*/
